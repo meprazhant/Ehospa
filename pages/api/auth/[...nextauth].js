0e4f8372d2545bada266bd29de99ae88005570f1
sijalabhatttarai <sijalabhattarai012@gmail.com>
2023-06-26 19:27:43 +0545

    login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import NextAuth from 'next-auth/next';
+import  GoogleProvider  from 'next-auth/providers/google';
+import  FacebookProvider  from 'next-auth/providers/facebook';
 
 export default NextAuth({
   providers: [
-    Providers.Google({
+    GoogleProvider({
       clientId: '498599588472-udm7s16pedts3t4nkaenfue1h7t1k0rv.apps.googleusercontent.com',
       clientSecret: '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',
     }),
-    Providers.Facebook({
+    FacebookProvider({
       clientId: '',
       clientSecret: '',
     }),
